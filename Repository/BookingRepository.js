6c7634aa135f9e0648f6766ddd2b18c766e02ac2
Jmytwenty8 <jmy2898@gmail.com>
2023-11-22 11:52:34 +0530

    Add missing try catch blocks for better error handling.

--- Repository/BookingRepository.js
+++ Repository/BookingRepository.js
@@ -1,22 +1,38 @@
 import { bookings } from "../Models/Booking.js";
 
 const getOneBooking = async (data) => {
-  const booking = await bookings.findOne({ _id: data.id });
-  return booking;
+  try {
+    const booking = await bookings.findOne({ _id: data.id });
+    return booking;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const getAllBookings = async () => {
-  const bookingList = await bookings.find({});
-  return bookingList;
+  try {
+    const bookingList = await bookings.find({});
+    return bookingList;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const getAllBookingsByUser = async (data) => {
-  const bookingList = await bookings.find({ userId: data.userId });
-  return bookingList;
+  try {
+    const bookingList = await bookings.find({ userId: data.userId });
+    return bookingList;
+  } catch (err) {
+    throw err;
+  }
 };
 const createBooking = async (data) => {
-  const booking = await bookings.create(data);
-  return booking;
+  try {
+    const booking = await bookings.create(data);
+    return booking;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const removeBooking = async (data) => {
--- Repository/TheaterRepository.js
+++ Repository/TheaterRepository.js
@@ -1,28 +1,48 @@
 import { theaters } from "../Models/Theaters.js";
 
 const getOneTheater = async (data) => {
-  const theater = await theaters.findOne({ _id: data.id });
-  return theater;
+  try {
+    const theater = await theaters.findOne({ _id: data.id });
+    return theater;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const getAllTheaters = async () => {
-  const theatersList = await theaters.find({});
-  return theatersList;
+  try {
+    const theatersList = await theaters.find({});
+    return theatersList;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const createTheater = async (data) => {
-  const theater = await theaters.create(data);
-  return theater;
+  try {
+    const theater = await theaters.create(data);
+    return theater;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const removeTheater = async (data) => {
-  const removedTheater = await theaters.deleteOne(data);
-  return removedTheater;
+  try {
+    const removedTheater = await theaters.deleteOne(data);
+    return removedTheater;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const updateTheater = async (theater, data) => {
-  const newTheater = await theaters.findOneAndUpdate(theater, data);
-  return newTheater;
+  try {
+    const newTheater = await theaters.findOneAndUpdate(theater, data);
+    return newTheater;
+  } catch (err) {
+    throw err;
+  }
 };
 
 export const TheaterRepository = {
